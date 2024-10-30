e7d8b6c5929b3f23e7b2eae983d540754d15ac7f
Rene Groeschke <rene@elastic.co>
2024-10-30 08:20:16 +0100

    Fix spotless after auto update of wolfi image

--- build-tools-internal/src/main/java/org/elasticsearch/gradle/internal/DockerBase.java
+++ build-tools-internal/src/main/java/org/elasticsearch/gradle/internal/DockerBase.java
@@ -35,6 +35,7 @@ public enum DockerBase {
         "-wolfi",
         "apk"
     ),
+    // spotless:on
 
     // Based on WOLFI above, with more extras. We don't set a base image because
     // we programmatically extend from the Wolfi image.
